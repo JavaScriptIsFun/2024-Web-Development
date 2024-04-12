704050e369fe83237b8ae06cc424a22b69ee5341
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2024-04-12 18:40:52 +0900

    2024/04/12
    Updates to section 14

---
+++ Section 14/Section 14 - 83.js
@@ -0,0 +1,9 @@
+var yourMessage = "Hello";
+var yourName = "Andre";
+var length = 100;
+
+prompt("yourMessage");
+/* prompt(yourMessage); */
+alert("You" + " have" + " written " + length.length + " characters");
+
+// You have written 100 characters, you have -10 characters left.
