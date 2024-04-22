538586d4d1efb96ee9e8d4f1efd8e9f1b48d6f94
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2024-04-23 07:13:09 +0900

    2024/23/04
    Updates to section 14

--- Section 14/Section 14 - 84.js
+++ Section 14/Section 14 - 84.js
@@ -1,2 +1,17 @@
-var name = "Andre";
-console.log(name.slice(0,1));
+// var name = "Andre";
+// console.log(name.slice(1,4));
+
+
+// Cut tweet down to 10 characters
+
+var tweet = prompt("Your message!");
+var tweetCount = tweet.length;
+var tweetLength = tweet.slice(0,10);
+
+/* alert("You have written " + tweetCount + " characters, you have  +(20 - tweetCount) + " characters remaining.");
+*/
+// alert("Your tweet lenght is " + tweetCount + " characters.");
+alert(tweetLength);
+
+// or
+alert(prompt("Your Tweet!").slice(0,10));
---
+++ Section 14/Section 14 - 85.js
@@ -0,0 +1,5 @@
+var name = prompt("What is your name?");
+var nameUpper = name.toUpperCase(1);
+//var firstLetter = slice(0,1);
+
+alert(("Hello").nameUpper(0,1));
