bd4916da9bb0d3afa7f4c5f4f04bcaa672d988a9
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2024-04-23 23:13:43 +0900

    2024/23/04
    Updates to section 14

--- Section 14/Section 14 - 85.js
+++ Section 14/Section 14 - 85.js
@@ -1,5 +1,5 @@
 var name = prompt("What is your name?");
-var nameUpper = name.toUpperCase(1);
+var nameUppercase = name.toUpperCase();
 //var firstLetter = slice(0,1);
 
-alert(("Hello").nameUpper(0,1));
+alert(("Hello").nameUppercase);
